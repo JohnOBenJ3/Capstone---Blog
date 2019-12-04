244f42e53ff9bb00ce4da0c9c4d8d0c8165dc204
Orrin Johnson <orrinjohnson@Orrins-MacBook-Air.local>
2019-12-04 13:45:33 -0700

    created a superuser for access to the admin site

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+# need to make a model for the Blog Post 
+class Post(model.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    
+    def __str__(self):
+        return self.title
