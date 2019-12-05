a10557eeb924a7fce2b26b6695351ef8e6813d46
Orrin Johnson <orrinjohnson@Orrins-MacBook-Air.local>
2019-12-05 09:32:10 -0700

    added a view to the views.py file for the homepage

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+# These are the same as the urlpatterns in the django_blog urls file.
+# But, you don't need the admin url in this file because it is already
+# in the main urls file.
+urlpatterns = [
+    path('', views.home, name='blog-home')
+]
